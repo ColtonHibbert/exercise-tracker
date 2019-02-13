ae5b4fa6ac78e9dcc1bfa8bbf54aea6d7caaaa59
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-02-12 19:46:57 -0700

    exercise-card

--- src/components/exercisecard/ExerciseCard.js
+++ src/components/exercisecard/ExerciseCard.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const ExerciseCard = (props) => {
 	return (
-		<div className="w-100 flex">
-			
+		<div className="w-100 flex flex-column">
+			<div className="w-100">Exercise</div>
+			<div className="w-100">Sets</div>
+			<div className="w-100">Reps</div>
+			<div className="w-100">Total Weight</div>
 		</div>
-		);
+		); 
 }
 
 export default ExerciseCard;
